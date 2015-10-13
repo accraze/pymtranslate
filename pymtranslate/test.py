d3bed91410c9ddb970d012844183211c7e849e56
accraze <accraze@gmail.com>
2015-10-12 23:22:11 -0700

    updating test to use refactor init

--- pymtranslate/test.py
+++ pymtranslate/test.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 import sys
-# sys.path.append('..')
+
 from translator import Translator
 
 english = 'pymtranslate/data/short.en'
@@ -12,21 +12,21 @@ wordList = 'pymtranslate/data/devwords'
 class TestTranslationFunctions(unittest.TestCase):
 
     def test_init(self):
-        t = Translator(['', english, foreign, wordList])
+        t = Translator(english, foreign, wordList)
         self.assertIn('the dog', t.en_dict)
         self.assertIsNot(t.en_words, [])
         self.assertIn('der Hund', t.de_dict)
         self.assertIsNot(t.de_words, [])
 
     def test_tef_init(self):
-        t = Translator(['', english, foreign, wordList])
+        t = Translator(english, foreign, wordList)
         self.assertFalse(t.transmissions)  # empty object
         t.initTef()
         self.assertEquals(t.transmissions, {'bus': {"der": 0.5, 'Bus': 0.5}, 'the': {"der": 0.2, 'Hund': 0.2, 'die': 0.2, 'Bus': 0.2, 'Katze': 0.2},
                                             'dog': {'Hund': 0.5, 'der': 0.5}, 'cat': {'die': 0.5, 'Katze': 0.5}})  # contains probabdie matches
 
     def test_EM_transitions(self):
-        t = Translator(['', english, foreign, wordList])
+        t = Translator(english, foreign, wordList)
         self.assertFalse(t.countef)  # empty object for counting mappings
         self.assertFalse(t.totalf)  # empty object
         t.initTef()
